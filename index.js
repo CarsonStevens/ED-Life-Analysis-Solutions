7c6348ead99938e46f7cf2893cc11d080f363593
carsonstevens <carsonstevens@mines.edu>
2020-05-14 20:07:35 -0600

    Hello World working on Express Server with CSS and file structure

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.engine('html', require('ejs').renderFile);
+// Use the built-in express middleware for serving static files from './public'
+app.use(express.static(__dirname + '/public'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
+// [END gae_flex_node_static_files]
+module.exports = app;
