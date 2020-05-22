cdf4bbd3a3acc6de2fcfecef3ea4b0e17dfdaa71
carsonstevens <carsonstevens@mines.edu>
2020-05-22 02:25:52 -0600

    Started to make the entry containers and fixed tab navigation when selected tab is clicked on.

--- public/javascript/entry-log.js
+++ public/javascript/entry-log.js
@@ -2,5 +2,82 @@
 
 $(document).ready(function() {
 
-  
+  //   // lazy: {
+  //   //   loadPrevNext: true,
+  //   //   loadPrevNextAmount: 2,
+  //   //   loadOnTransitionStart: false,
+  //   //   elementClass: 'swiper-lazy',
+  //   //   loadingClass: 'swiper-lazy-loading',
+  //   //   loadedClass: 'swiper-lazy-loaded',
+  //   //   preloaderClass: 'swiper-lazy-preloader'
+  //   // },
+  //   // preloadImages: false,
+  //   // keyboard: {
+  //   //   enabled: true,
+  //   //   onlyInViewport: false,
+  //   // }
+
+  var swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    speed: 500,
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 500,
+    freeMode: true,
+    freeModeMomentumRatio: 0.6,
+    freeModeMomentumVelocityRatio: 0.8,
+    freeModeSticky: true,
+    observer: true,
+    observeParents: true,
+    on: {
+      init: function () {
+        console.log('swiper initialized');
+      },
+      slideChange: function () {
+        console.log('slide changed');
+      }
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+      progressbarOpposite: false,
+      // renderBullet: function (index, className) {
+      //       return '<span class="' + className + '">' + (index + 1) + '</span>';
+      // },
+      renderProgressbar: function (progressbarFillClass) {
+          return '<span class="' + progressbarFillClass + '"></span>';
+      },
+      progressbarFillClass: 'swiper-pagination-progressbar-fill',
+      bulletClass: "swiper-pagination-bullet",
+      bulletActiveClass: "swiper-pagination-bullet-active",
+      currentClasss: "swiper-pagination-current",
+      clickableClass: "swiper-pagination-clickable",
+      lockClass: "swiper-pagination-lock"
+    },
+
+    grabCursor: true,
+    noSwipingSelector: 'input',
+
+    watchSlidesProgress: true,
+    paginationClickable: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: false
+    },
+    a11y: {
+      prevSlideMessage: 'Previous slide',
+      nextSlideMessage: 'Next slide',
+    }
+  });
+
 });
--- public/javascript/navigation.js
+++ public/javascript/navigation.js
@@ -4,18 +4,20 @@ $(document).ready(function() {
 
   // Dashboard tab switching
   $(".tab").on("click", function() {
-    $(".container").css({"display": "none"});
     if($(this).data("tab") == "dashboard" && !$("#dashboard").hasClass("selected-tab")) {
+      $(".container").css({"display": "none"});
       $(".selected-tab").addClass("unselected-tab").removeClass("selected-tab");
       $("#dashboard").addClass("selected-tab").removeClass("unselected-tab");
       $("#dashboard-container").css({"display": "grid"});
     }
     else if ($(this).data("tab") == "entry" && !($("#entry").hasClass("selected-tab"))) {
+      $(".container").css({"display": "none"});
       $(".selected-tab").addClass("unselected-tab").removeClass("selected-tab");
       $("#entry").addClass("selected-tab").removeClass("unselected-tab");
       $("#entry-container").css({"display": "grid"});
     }
     else if ($(this).data("tab") == "settings" && !$("#settings").hasClass("selected-tab")) {
+      $(".container").css({"display": "none"});
       $(".selected-tab").addClass("unselected-tab").removeClass("selected-tab");
       $("#settings").addClass("selected-tab").removeClass("unselected-tab");
       $("#settings-container").css({"display": "flex"});
