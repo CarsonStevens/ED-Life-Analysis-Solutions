3381a8ebda5c340f0e8f9e19d969b90cd5bb1703
carsonstevens <carsonstevens@mines.edu>
2020-05-22 19:04:38 -0600

    Worked more on the log entry UI and added the file uploading files currently connectly via ajax

--- public/javascript/entry-log.js
+++ public/javascript/entry-log.js
@@ -31,6 +31,8 @@ $(document).ready(function() {
     freeModeSticky: true,
     observer: true,
     observeParents: true,
+    preventInteractionOnTransition: true,
+    noSwiping: false,
     on: {
       init: function () {
         console.log('swiper initialized');
@@ -78,6 +80,28 @@ $(document).ready(function() {
       prevSlideMessage: 'Previous slide',
       nextSlideMessage: 'Next slide',
     }
+  });
+
+
+  function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+      textbox.addEventListener(event, function() {
+        if (inputFilter(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        } else {
+          this.value = "";
+        }
+      });
+    });
+  }
+
+  setInputFilter(document.getElementById("weight-input-manual"), function(value) {
+    return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
   });
 
 });
